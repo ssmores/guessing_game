81d50b6ed19cc4b2ac92dced96d2e46dee062f0e
Hackbright Student <info@hackbrightacademy.com>
2016-07-06 13:00:56 -0700

    Ask player to guess # between 1-100

--- game.py
+++ game.py
@@ -6,3 +6,7 @@ print "Greetings!"
 player_name = raw_input("What is your name friend? ")
 
 randomly_generated_number = random.randint(1, 100)
+
+player_guess = int(raw_input("Guess a number between 1 and 100. "))
+
+# while player_guess != randomly_generated_number:
