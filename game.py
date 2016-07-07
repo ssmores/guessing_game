21947f57651ac9e04ffe6b355f9f80b9acedbc37
Hackbright Student <info@hackbrightacademy.com>
2016-07-06 17:05:25 -0700

    Added while loop to evaluate player's guess against random #

--- game.py
+++ game.py
@@ -7,6 +7,21 @@ player_name = raw_input("What is your name friend? ")
 
 randomly_generated_number = random.randint(1, 100)
 
+print randomly_generated_number
+
 player_guess = int(raw_input("Guess a number between 1 and 100. "))
 
-# while player_guess != randomly_generated_number:
+while player_guess != randomly_generated_number:
+    
+    if player_guess < 1 or player_guess > 100:
+        print "Try again, between 1 and 100 only."
+        player_guess = int(raw_input("Guess a number between 1 and 100. ")) 
+    elif player_guess > randomly_generated_number:
+        print "You are too high, try again."
+        player_guess = int(raw_input("Guess a number between 1 and 100. "))
+    elif player_guess < randomly_generated_number:
+        print "You are too low, try again."
+        player_guess = int(raw_input("Guess a number between 1 and 100. "))
+
+
+print "Good job, you did it!"
