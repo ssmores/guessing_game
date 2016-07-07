df1e0d5e367ce2c69927b33c4398bf95aee0b416
Hackbright Student <info@hackbrightacademy.com>
2016-07-06 18:14:54 -0700

    Started to add a try/except for non-integer inputs, which is currently commented out.

--- game.py
+++ game.py
@@ -5,14 +5,24 @@ print "Greetings!"
 
 player_name = raw_input("What is your name friend? ")
 
-randomly_generated_number = random.randint(1, 100)
+randomly_generated_number = random.randint(1, 100) 
 
-player_guess = int(raw_input("Guess a number between 1 and 100. "))
+player_guess = raw_input("Guess a number between 1 and 100. ")
+
+# Unfinished function where we wanted to try the try function. 
+# def is_an_integer(guess): 
+#     player_guess = raw_input("Guess a number between 1 and 100. ")
+#     try:
+#         type(player_guess) == int
+
+#     except Exception, e:
+#         raise e
+
 
 while player_guess != randomly_generated_number:
     
     if player_guess < 1 or player_guess > 100:
-        print "Try again, between 1 and 100 only."
+        print "Try again, between 1 and 100 only this time."
         player_guess = int(raw_input("Guess a number between 1 and 100. ")) 
     elif player_guess > randomly_generated_number:
         print "You are too high, try again."
