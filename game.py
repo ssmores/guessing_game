168ad0887a5731fbf99d2928b8c1c99f2adf6fb3
Hackbright Student <info@hackbrightacademy.com>
2016-07-06 17:08:34 -0700

    Removed print statement for the randomly generated #

--- game.py
+++ game.py
@@ -7,8 +7,6 @@ player_name = raw_input("What is your name friend? ")
 
 randomly_generated_number = random.randint(1, 100)
 
-print randomly_generated_number
-
 player_guess = int(raw_input("Guess a number between 1 and 100. "))
 
 while player_guess != randomly_generated_number:
