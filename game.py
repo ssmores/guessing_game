a6a589e5c4609ceac9a34a986c99c1ac5f563e12
Stefanie Moy <ssmores@hotmail.com>
2016-07-10 07:27:55 +0000

    Type casted initial player guess as an integer, to ensure the first if statement ran accurately.

--- game.py
+++ game.py
@@ -7,7 +7,7 @@ player_name = raw_input("What is your name friend? ")
 
 randomly_generated_number = random.randint(1, 100) 
 
-player_guess = raw_input("Guess a number between 1 and 100. ")
+player_guess = int(raw_input("Guess a number between 1 and 100. "))
 
 # Unfinished function where we wanted to try the try function. 
 # def is_an_integer(guess): 
@@ -18,11 +18,10 @@ player_guess = raw_input("Guess a number between 1 and 100. ")
 #     except Exception, e:
 #         raise e
 
-
 while player_guess != randomly_generated_number:
     
     if player_guess < 1 or player_guess > 100:
-        print "Try again, between 1 and 100 only this time."
+        print "Please try again, and input a number between 1 and 100 only this time."
         player_guess = int(raw_input("Guess a number between 1 and 100. ")) 
     elif player_guess > randomly_generated_number:
         print "You are too high, try again."
@@ -32,4 +31,4 @@ while player_guess != randomly_generated_number:
         player_guess = int(raw_input("Guess a number between 1 and 100. "))
 
 
-print "Good job, you did it!"
+print "The number was %i! Good job %s, you did it!" % (player_guess, player_name)
